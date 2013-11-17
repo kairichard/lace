3cc17629dcbb33999368b0c222418423cd63f1f5
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2013-11-17 12:17:08 +0100

    figuring out lib path.

--- lib/koon.rb
+++ lib/koon.rb
@@ -1,1 +1,7 @@
+std_trap = trap("INT") { exit! 130 } # no backtrace thanks
+KOON_FILE = ENV["KOON_FILE"]
+
+require 'pathname'
+KOON_LIB_PATH = Pathname.new(__FILE__).realpath.dirname.parent.join("lib").to_s
+puts KOON_LIB_PATH
 puts "hello"
