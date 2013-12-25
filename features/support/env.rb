dbe0be0eaae25eb22f1a8dfb2af4760e1e7d36ad
Kai Richard Koenig <kai@kairichardkoenig.com>
2013-12-25 18:04:16 +0100

    Renaming env variable

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,15 +1,15 @@
 require 'aruba/cucumber'
 
 Before do
-  @__old_kaktis_folder = ENV["KAKITS_FOLDER"]
+  @__old_kaktis_folder = ENV["ZIMT_FOLDER"]
   @__old_home = ENV["HOME"]
   @installed_cassias = "installed_cassias"
 
-  ENV["KAKITS_FOLDER"] = File.expand_path('tmp/aruba/' + @installed_cassias)
+  ENV["ZIMT_FOLDER"] = File.expand_path('tmp/aruba/' + @installed_cassias)
   ENV["HOME"] = File.expand_path("tmp/aruba/HOME")
 
   in_current_dir do
     _mkdir ENV["HOME"]
-    _mkdir ENV["KAKITS_FOLDER"]
+    _mkdir ENV["ZIMT_FOLDER"]
   end
 end
