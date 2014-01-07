ea18624385180b7a4fc3849b11f65e1de09c5e55
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-01-07 22:04:26 +0100

    Removing unused method

--- lib/extend/pathname.rb
+++ lib/extend/pathname.rb
@@ -1,5 +1,5 @@
 require 'pathname'
-
+# borrowed from brew.sh
 # we enhance pathname to make our code more readable
 class Pathname
 
@@ -134,55 +134,6 @@ class Pathname
     false
   else
     (dirname+link).exist?
-  end
-
-  # perhaps confusingly, this Pathname object becomes the symlink pointing to
-  # the src paramter.
-  def make_relative_symlink src
-    src = Pathname.new(src) unless src.kind_of? Pathname
-
-    self.dirname.mkpath
-    Dir.chdir self.dirname do
-      # NOTE only system ln -s will create RELATIVE symlinks
-      quiet_system 'ln', '-s', src.relative_path_from(self.dirname), self.basename
-      if not $?.success?
-        if self.exist?
-          raise <<-EOS.undent
-            Could not symlink file: #{src.expand_path}
-            Target #{self} already exists. You may need to delete it.
-            To force the link and overwrite all other conflicting files, do:
-              brew link --overwrite formula_name
-
-            To list all files that would be deleted:
-              brew link --overwrite --dry-run formula_name
-            EOS
-        # #exist? will return false for symlinks whose target doesn't exist
-        elsif self.symlink?
-          raise <<-EOS.undent
-            Could not symlink file: #{src.expand_path}
-            Target #{self} already exists as a symlink to #{readlink}.
-            If this file is from another formula, you may need to
-            `brew unlink` it. Otherwise, you may want to delete it.
-            To force the link and overwrite all other conflicting files, do:
-              brew link --overwrite formula_name
-
-            To list all files that would be deleted:
-              brew link --overwrite --dry-run formula_name
-            EOS
-        elsif !dirname.writable_real?
-          raise <<-EOS.undent
-            Could not symlink file: #{src.expand_path}
-            #{dirname} is not writable. You should change its permissions.
-            EOS
-        else
-          raise <<-EOS.undent
-            Could not symlink file: #{src.expand_path}
-            #{self} may already exist.
-            #{dirname} may not be writable.
-            EOS
-        end
-      end
-    end
   end
 
   def / that
