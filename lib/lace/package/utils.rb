0446813347eb185703b98d59de791d145240ecb1
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-08-07 19:56:36 +0200

    Add parenthesis where applicable

--- lib/lace/package/utils.rb
+++ lib/lace/package/utils.rb
@@ -10,7 +10,7 @@ class PackageUtils
   end
 
   def self.remove package_name
-    package = Package.new package_name, false
+    package = Package.new(package_name, false)
     raise CannotRemoveActivePackage.new if package.is_active?
     ohai "Removing"
     FileUtils.rm_rf package.path
@@ -18,7 +18,7 @@ class PackageUtils
 
   def self.setup package_name
     begin
-      package = Package.new package_name, ARGV.first
+      package = Package.new(package_name, ARGV.first)
       package.activate!
       package.setup
     rescue FlavorError => e
@@ -28,23 +28,23 @@ class PackageUtils
   end
 
   def self.deactivate package_name
-    package = Package.new package_name, ARGV.first
+    package = Package.new(package_name, ARGV.first)
     raise NonActiveFlavorError.new unless package.is_active? || ARGV.force?
     ohai "Deactivating"
     package.deactivate!
   end
 
   def self.activate package_name
-    package = Package.new package_name, ARGV.first
+    package = Package.new(package_name, ARGV.first)
     raise AlreadyActiveError.new if Package.new(package_name, false).is_active?
     ohai "Activating"
     package.activate!
   end
 
   def self.update package_name
-    package = Package.new package_name, false
+    package = Package.new(package_name, false)
     raise OnlyGitReposCanBeUpdatedError.new unless package.is_git_repo?
-    updater = GitUpdateStrategy.new package_name
+    updater = GitUpdateStrategy.new(package_name)
     was_active_before_update = package.is_active?
     self.deactivate(package_name) if was_active_before_update
     ohai "Updating"
