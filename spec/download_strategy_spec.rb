1dd3ce84eaffb75fc1c68b630e5a254ed20b7546
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2015-06-03 15:58:27 +0200

    Add more test for if files exist

--- spec/download_strategy_spec.rb
+++ spec/download_strategy_spec.rb
@@ -1,13 +1,29 @@
 require 'lace'
 require 'lace/download_strategy'
 require "extend/pathname"
+require 'fileutils'
 
 describe "DownloadStrategyDetector" do
   it "detects a strategy for github if package name is the format of user/repo" do
-    uri = 'someuser/demorepository'
+    uri = 'someuser/repo'
     strategy = DownloadStrategyDetector.detect(uri)
     expect(strategy).to be AbbrevGitDownloadStrategy
   end
+
+  it "detects a strategy for localfile if file actually exists" do
+    uri = 'someuser/file'
+    FileUtils::mkdir_p uri
+    strategy = DownloadStrategyDetector.detect(uri)
+    expect(strategy).to be LocalFileStrategy
+  end
+
+  it "detects a strategy for localfile if file actually exists without extension" do
+    uri = 'someuser/repo'
+    FileUtils::mkdir_p uri
+    strategy = DownloadStrategyDetector.detect(uri+".git")
+    expect(strategy).to be AbbrevGitDownloadStrategy
+  end
+end
 
 describe "AbbrevGitDownloadStrategy" do
 
