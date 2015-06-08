f5216986e17f5f97e17f456e0404c4ae673f9ff8
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2015-06-08 22:31:00 +0200

    Add test and fix for wrong number of args issue
    This happens when using the abbrev version of a github url

---
+++ spec/package_utils_spec.rb
@@ -0,0 +1,13 @@
+require 'lace'
+require 'lace/download_strategy'
+require 'lace/package'
+
+describe 'PackageUtils' do
+  it "fetches and abbriviated github-url" do
+    expect_any_instance_of(AbbrevGitDownloadStrategy).to receive(:fetch){
+      true
+    }
+    PackageUtils.fetch("kairichard/somewhere", nil)
+  end
+end
+
